35916d668aaa0551f40a68918b29c6e63cd9c8be
Ricardo Javier Barrios Díaz <javier.barrios@eviden.com>
2023-09-26 22:25:26 +0100

    refactor(common): run formatter

--- packages/common/jest.config.ts
+++ packages/common/jest.config.ts
@@ -3,8 +3,8 @@ export default {
   displayName: 'common',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/common',
+  coverageDirectory: '../../coverage/packages/common'
 };
