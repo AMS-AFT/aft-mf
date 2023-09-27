ef2ca6e1ca1728b0730eee4a8a64986691b26c67
Ricardo Javier Barrios Díaz <javier.barrios@eviden.com>
2023-09-27 12:46:22 +0100

    feat(common): add stringToObject

---
+++ packages/common/src/lib/string-to-object.function.spec.ts
@@ -0,0 +1,41 @@
+import { stringToObject } from './string-to-object.function';
+
+describe('stringToObject', () => {
+  const config = { prop: ';', val: ':' };
+  const obj = { a: '0', b: '0' };
+
+  it(`returns the string as object`, () => {
+    const value = 'a=0,b=0';
+    expect(stringToObject(value)).toEqual(obj);
+  });
+
+  it(`returns empty object if invalid separators`, () => {
+    const value = 'a:0;b:0';
+    expect(stringToObject(value)).toEqual({});
+  });
+
+  it(`returns the string as object with custom config`, () => {
+    const value = 'a:0;b:0';
+    expect(stringToObject(value, config)).toEqual(obj);
+  });
+
+  it(`returns the string as object avoiding extra spaces`, () => {
+    const value = 'a = 0 , b = 0';
+    expect(stringToObject(value)).toEqual(obj);
+  });
+
+  it(`returns the string with multiple property separators`, () => {
+    const value = 'a=0 , , , b=0';
+    expect(stringToObject(value)).toEqual(obj);
+  });
+
+  it(`returns the string with multiple value separators`, () => {
+    const value = 'a=0,b=0 = 0';
+    expect(stringToObject(value)).toEqual({ a: '0', b: '0 = 0' });
+  });
+
+  it(`returns the string mixing separators`, () => {
+    const value = 'a=0, b:0';
+    expect(stringToObject(value)).toEqual({ a: '0' });
+  });
+});
---
+++ packages/common/src/lib/string-to-object.function.ts
@@ -0,0 +1,55 @@
+/**
+ * The `stringToObject` configuration object.
+ */
+export interface StringToObjectConfig {
+  /**
+   * The string used to separate properties.
+   *
+   * Defaults to `','`.
+   */
+  prop: string;
+
+  /**
+   * The string used to separate the key from the value.
+   *
+   * Defaults to `'='`.
+   */
+  val: string;
+}
+
+/**
+ * Converts a key value pairs string to object.
+ * @param value The key value pairs string to convert.
+ * @param config The configuration object. Defaults to `{ prop: ',', val: '=' }`.
+ * @returns The key value pairs string as object or empty object if invalid config.
+ * @publicApi
+ * @example
+ * ```ts
+ * stringToObject('a=0, b=0'); // { a:'0', b:'0' }
+ * stringToObject('a:0; b:0'); // {}
+ * stringToObject('a:0; b:0', { prop: ';', val: ':' }); // { a:'0', b:'0' }
+ * stringToObject('a=0 , , , b=0'); // { a:'0', b:'0' }
+ * stringToObject('a=0, b=0 = 0'); // { a:'0', b:'0 = 0' }
+ * stringToObject('a=0, b:0'); // { a:'0' }
+ * ```
+ */
+export function stringToObject(
+  value: string,
+  config: StringToObjectConfig = { prop: ',', val: '=' }
+): Record<string, string> {
+  const props: string[] = value
+    .split(config.prop)
+    .map((property: string): string => property.trim())
+    .filter((value: string): boolean => value.length > 0);
+
+  return props.reduce((obj: Record<string, string>, prop: string) => {
+    const kv = prop.split(config.val);
+    const key = kv.shift() as string;
+
+    if (kv.length > 0) {
+      obj[key.trim()] = kv.join(config.val).trim();
+    }
+
+    return obj;
+  }, {});
+}
