00337bfc1d1dc2fe6b26baada17d207f052744f1
Ricardo Javier Barrios Díaz <javier.barrios@eviden.com>
2023-09-27 12:48:53 +0100

    feat(common): export common functions

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/common';
+export * from './lib/random-between.function';
+export * from './lib/string-to-object.function';
+export * from './lib/time-conversion.constant';
+export * from './lib/value-or-fn.function';
