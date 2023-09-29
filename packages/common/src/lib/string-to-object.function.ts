6a58763a89865ccc1b52e94f32e52c00022733bf
Ricardo Javier Barrios Díaz <javier.barrios@eviden.com>
2023-09-29 19:32:15 +0100

    docs(common): update documentation

--- packages/common/src/lib/string-to-object.function.ts
+++ packages/common/src/lib/string-to-object.function.ts
@@ -1,5 +1,6 @@
 /**
  * The `stringToObject` configuration object.
+ * @publicApi
  */
 export interface StringToObjectConfig {
   /**
--- packages/common/src/lib/value-or-fn.function.ts
+++ packages/common/src/lib/value-or-fn.function.ts
@@ -2,11 +2,14 @@
 
 /**
  * Matches a value or a function that returns the value.
+ * @template T The expected value type.
+ * @publicApi
  */
 export type ValueOrFn<T> = T | ((...args: any[]) => T);
 
 /**
  * Solves for a value that can be a value or a function that returns the value.
+ * @template T The expected value type.
  * @param value The value or function to solve.
  * @param args The arguments needed to resolve the function.
  * @returns The value or the result of executing the function.
