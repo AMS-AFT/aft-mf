2cad262f15a94286973cbc6ff70715441d643a93
Ricardo Javier Barrios Díaz <javier.barrios@eviden.com>
2023-09-30 17:02:06 +0100

    feat(common): update valueOrFn and separate types

--- packages/common/src/lib/value-or-fn.function.ts
+++ packages/common/src/lib/value-or-fn.function.ts
@@ -1,18 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
-/**
- * Matches a value or a function that returns the value.
- * @template T The expected value type.
- * @publicApi
- */
-export type ValueOrFn<T> = T | ((...args: any[]) => T);
+import { ValueOrFn } from './value-or-fn.type';
 
 /**
  * Solves for a value that can be a value or a function that returns the value.
- * @template T The expected value type.
+ * @template T The value type.
+ * @template Args The arguments type. Defaults to `any`.
  * @param value The value or function to solve.
  * @param args The arguments needed to resolve the function.
- * @returns The value or the result of executing the function.
+ * @returns The value or the result of executing the function with the args.
  * @publicApi
  * @example
  * ```ts
@@ -23,6 +18,6 @@ export type ValueOrFn<T> = T | ((...args: any[]) => T);
  * valueOrFn((a, b) => a + b, 1, 1); // 2
  * ```
  */
-export function valueOrFn<T>(value: ValueOrFn<T>, ...args: any[]): T {
+export function valueOrFn<T, Args = any>(value: ValueOrFn<T, Args>, ...args: Args[]): T {
   return value instanceof Function ? value(...args) : value;
 }
