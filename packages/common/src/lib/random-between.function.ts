6bdbb4ffaf048436e631f73a200087dc0ca28755
Ricardo Javier Barrios Díaz <javier.barrios@eviden.com>
2023-09-30 17:03:29 +0100

    feat(common): update randomBetween

--- packages/common/src/lib/random-between.function.ts
+++ packages/common/src/lib/random-between.function.ts
@@ -1,38 +1,44 @@
 /**
  * Gets a random integer between two values, both included.
  *
- * The lower value is rounded down and the bigger one rounded up if they are decimals.
- * @param value1 The first value.
- * @param value2 The second value.
- * @returns A random integer between the two values, with MIN_SAFE_INTEGER and MAX_SAFE_INTEGER as limits.
- * @throws TypeError if any value is NaN.
- * @see Number.MIN_SAFE_INTEGER
- * @see Number.MAX_SAFE_INTEGER
+ * Will swap values if `min` is bigger than `max`.
+ * The lower value will be rounded down if it's decimal, with a minimum of `MIN_SAFE_INTEGER`.
+ * The bigger value will be rounded up if it's decimal, with a maximum of `MAX_SAFE_INTEGER`.
+ * @param min The minimum value.
+ * @param max The maximum value.
+ * @returns A random integer between the two values.
+ * @throws `TypeError` if any value is `NaN`.
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MIN_SAFE_INTEGER | MIN_SAFE_INTEGER }
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER | MAX_SAFE_INTEGER }
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError | TypeError }
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN | NaN }
  * @publicApi
  * @example
  * ```ts
  * randomBetween(200, 300); // 242
  * randomBetween(300, 200); // 231
- * randomBetween(200.1, 299.1); // 200 - 300
- * randomBetween(NEGATIVE_INFINITY, POSITIVE_INFINITY); // MIN_SAFE_INTEGER - MAX_SAFE_INTEGER
+ * randomBetween(200.1, 299.1);
+ * // random from 200 to 300, both included
+ * randomBetween(NEGATIVE_INFINITY, POSITIVE_INFINITY);
+ * // random from MIN_SAFE_INTEGER to MAX_SAFE_INTEGER, both included
  * randomBetween(NaN, 300); // throws TypeError
  * ```
  */
-export function randomBetween(value1: number, value2: number): number {
-  if (Number.isNaN(value1) || Number.isNaN(value2)) {
+export function randomBetween(min: number, max: number): number {
+  if (Number.isNaN(min) || Number.isNaN(max)) {
     throw new TypeError();
   }
 
-  let min = value2 < value1 ? Math.floor(value2) : Math.floor(value1);
-  let max = value2 < value1 ? Math.ceil(value1) : Math.ceil(value2);
+  let realMin = max < min ? Math.floor(max) : Math.floor(min);
+  let realMax = max < min ? Math.ceil(min) : Math.ceil(max);
 
-  if (min < Number.MIN_SAFE_INTEGER) {
-    min = Number.MIN_SAFE_INTEGER;
+  if (realMin < Number.MIN_SAFE_INTEGER) {
+    realMin = Number.MIN_SAFE_INTEGER;
   }
 
-  if (max > Number.MAX_SAFE_INTEGER) {
-    max = Number.MAX_SAFE_INTEGER;
+  if (realMax > Number.MAX_SAFE_INTEGER) {
+    realMax = Number.MAX_SAFE_INTEGER;
   }
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (realMax - realMin + 1)) + realMin;
 }
